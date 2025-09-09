b991e832fdfe7efa5c53b81bb11f54cdc52f0bbb
BVerstaen <bverstaen04@gmail.com>
2025-09-09 09:12:37 +0200

    [ADD] Smoothing camera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,7 +8,12 @@ public class CameraController : MonoBehaviour
 
     [Header("References")]
     public Camera controlledCamera;
-    [SerializeField] private CameraConfiguration _cameraConfiguration;
+    [SerializeField] private CameraConfiguration _currentConfiguration;
+    [SerializeField] private CameraConfiguration _targetConfiguration;
+
+    [Header("Smoothing")]
+    [SerializeField] private float _smoothSpeed;
+    [SerializeField] private float _smoothTreshold = 1;
 
     [Header("Debug")]
     [SerializeField] private Color _debugColor;
@@ -17,7 +22,7 @@ public class CameraController : MonoBehaviour
 
     private void OnValidate()
     {
-        _cameraConfiguration.OnClampPitch();
+        _currentConfiguration.OnClampPitch();
     }
 
     private void Awake()
@@ -31,21 +36,29 @@ public class CameraController : MonoBehaviour
         Instance = this;
     }
 
+    private void Start()
+    {
+        _currentConfiguration = ComputeAverage();
+        _targetConfiguration = ComputeAverage();
+    }
+
     private void Update()
     {
+        _targetConfiguration = ComputeAverage();
+        SmoothCurrentConfigurationToTarget();
         ApplyConfiguration();
     }
 
     private void OnDrawGizmos()
     {
-        _cameraConfiguration.DrawGizmos(_debugColor);
+        _currentConfiguration.DrawGizmos(_debugColor);
     }
 
     private void ApplyConfiguration()
     {
-        controlledCamera.transform.position = _cameraConfiguration.GetPosition();
-        controlledCamera.transform.rotation = _cameraConfiguration.GetRotation();
-        controlledCamera.fieldOfView = _cameraConfiguration.fieldOfView;
+        controlledCamera.transform.position = _currentConfiguration.GetPosition();
+        controlledCamera.transform.rotation = _currentConfiguration.GetRotation();
+        controlledCamera.fieldOfView = _currentConfiguration.fieldOfView;
     }
 
     public void AddView(AView view)
@@ -108,5 +121,23 @@ public class CameraController : MonoBehaviour
             sum += new Vector2(Mathf.Cos(config.yaw * Mathf.Deg2Rad), Mathf.Sin(config.yaw * Mathf.Deg2Rad)) * view.weight;
         }
         return Vector2.SignedAngle(Vector2.right, sum);
+    }
+
+    private void SmoothCurrentConfigurationToTarget()
+    {
+        float currentSpeed = _smoothSpeed * Time.deltaTime;
+
+        if (currentSpeed < _smoothTreshold)
+        {
+            _currentConfiguration.yaw = _currentConfiguration.yaw + (_targetConfiguration.yaw -_currentConfiguration.yaw) * currentSpeed;
+            _currentConfiguration.pitch = _currentConfiguration.pitch + (_targetConfiguration.pitch - _currentConfiguration.pitch) * currentSpeed;
+            _currentConfiguration.roll = _currentConfiguration.roll + (_targetConfiguration.roll - _currentConfiguration.roll) * currentSpeed;
+
+            _currentConfiguration.pivot = _currentConfiguration.pivot + (_targetConfiguration.pivot - _currentConfiguration.pivot) * currentSpeed;
+            _currentConfiguration.distance = _currentConfiguration.distance + (_targetConfiguration.distance - _currentConfiguration.distance) * currentSpeed;
+            _currentConfiguration.fieldOfView = _currentConfiguration.fieldOfView + (_targetConfiguration.fieldOfView - _currentConfiguration.fieldOfView) * currentSpeed;
+        }
+        else
+            _currentConfiguration = _targetConfiguration;
     }
 }
