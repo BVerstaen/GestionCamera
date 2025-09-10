cbd6a39aa187babfc041097d8da11bc21d3cb446
BVerstaen <bverstaen04@gmail.com>
2025-09-10 08:58:02 +0200

    [ADD] AView Volume + ViewVolumeBlender
    En gros 1.1 et 1.2 du 2ème TP

---
+++ Assets/Scripts/Volume/AViewVolume.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class AViewVolume : MonoBehaviour
+{
+    public int priority = 0;
+    public AView view;
+
+    private int _Uid = 0;
+    public static int NextUid = 0;
+
+    protected bool IsActive {  get; private set; }
+
+    private void Awake()
+    {
+        _Uid = NextUid;
+        NextUid++;
+    }
+    public virtual float ComputeSelfWeight() { return 1.0f; }
+
+    protected void SetActive(bool isActive)
+    {
+        IsActive = isActive;
+
+        if (isActive)
+            ViewVolumeBlender.Instance.AddVolume(this);
+        else 
+            ViewVolumeBlender.Instance.RemoveVolume(this);
+    }
+}
---
+++ Assets/Scripts/Volume/ViewVolumeBlender.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewVolumeBlender : MonoBehaviour
+{
+    public static ViewVolumeBlender Instance;
+
+    private List<AViewVolume> _activeViewVolumes;
+    private Dictionary<AView, List<AViewVolume>> _volumesPerViews;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    public void AddVolume(AViewVolume a_volume)
+    {
+        _activeViewVolumes.Add(a_volume);
+
+        if (!_volumesPerViews.ContainsKey(a_volume.view))
+        {
+            _volumesPerViews.Add(a_volume.view, new List<AViewVolume>());
+            a_volume.view.SetActive(true);
+        }
+
+        _volumesPerViews[a_volume.view].Add(a_volume);
+    }
+
+    public void RemoveVolume(AViewVolume a_volume)
+    {
+        _activeViewVolumes.Remove(a_volume);
+
+        _volumesPerViews[a_volume.view].Remove(a_volume);
+        if(_volumesPerViews[a_volume.view].Count <= 0)
+        {
+            _volumesPerViews.Remove(a_volume.view);
+            a_volume.view.SetActive(false);
+        }
+    }
+}
