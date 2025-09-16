a114bef12e912fc3cf377c1f8c033c92781c7937
Nirgigo <titouan@dupuisland.net>
2025-09-16 10:12:33 +0200

    [MOD] FreeFollowView Gizmos

--- Assets/Scripts/FreeFollowCamera/Curve.cs
+++ Assets/Scripts/FreeFollowCamera/Curve.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 
+[Serializable]
 public class Curve
 {
     public Vector3 A;
@@ -20,6 +22,22 @@ public class Curve
 
     public void DrawGizmo(Color a_color, Matrix4x4 a_localToWorldMatrix)
     {
-        
+        Gizmos.color = a_color;
+
+        Gizmos.DrawSphere(a_localToWorldMatrix.MultiplyPoint(A), 0.1f);
+        Gizmos.DrawSphere(a_localToWorldMatrix.MultiplyPoint(B), 0.1f);
+        Gizmos.DrawSphere(a_localToWorldMatrix.MultiplyPoint(C), 0.1f);
+        Gizmos.DrawSphere(a_localToWorldMatrix.MultiplyPoint(D), 0.1f);
+
+        for (int i = 0; i < 30; i++)
+        {
+            Gizmos.DrawLine(GetPosition((i / 30f), a_localToWorldMatrix), GetPosition((i + 1) / 30f, a_localToWorldMatrix));
+        }
+
+        Gizmos.color = Color.Lerp(a_color, Color.black, 0.5f);
+
+        Gizmos.DrawLine(a_localToWorldMatrix.MultiplyPoint(A), a_localToWorldMatrix.MultiplyPoint(B));
+        Gizmos.DrawLine(a_localToWorldMatrix.MultiplyPoint(B), a_localToWorldMatrix.MultiplyPoint(C));
+        Gizmos.DrawLine(a_localToWorldMatrix.MultiplyPoint(C), a_localToWorldMatrix.MultiplyPoint(D));
     }
 }
