e209648c7089a1d18c632974432091f0b849a383
BVerstaen <bverstaen04@gmail.com>
2025-09-18 13:44:38 +0200

    [FIX] Fix collision

--- Assets/Scripts/Camera/CameraCollision.cs
+++ Assets/Scripts/Camera/CameraCollision.cs
@@ -1,29 +1,43 @@
 using UnityEngine;
-
 public class CameraCollision : MonoBehaviour
 {
     [Header("References")]
-    public GameObject targetObject;
+    [SerializeField] private GameObject _targetObject;
 
     [Header("Parameters")]
-    public float sphereCastRadius;
+    [SerializeField] private float _sphereCastRadius;
+    [SerializeField] private LayerMask _obstacleLayers;
+
+    private Vector3 _gizmosCameraPoint;
 
     private void OnValidate()
     {
-        if (targetObject == null)
-            targetObject = GameObject.FindGameObjectWithTag("Player");
+        if (_targetObject == null)
+            _targetObject = GameObject.FindGameObjectWithTag("Player");
     }
 
     public bool HasClearPathToTarget(Vector3 startPosition, out Vector3 hitPosition)
     {
         hitPosition = startPosition;
-        Vector3 targetPos = targetObject.transform.position;
-        Vector3 rayDirection = (targetPos - startPosition).normalized;
+        _gizmosCameraPoint = hitPosition;
+
+        Vector3 targetPos = _targetObject.transform.position;
+        Vector3 rayDirection = -(targetPos - startPosition).normalized;
         float rayDistance = Vector3.Distance(targetPos, startPosition);
-        if (Physics.SphereCast(startPosition, sphereCastRadius, rayDirection, out RaycastHit hitInfo, rayDistance))
+
+        //Check if something is in the way
+        if (Physics.SphereCast(targetPos, _sphereCastRadius, rayDirection, out RaycastHit hitInfo, rayDistance, _obstacleLayers))
         {
-            return hitInfo.collider.gameObject == targetObject;
+            hitPosition = hitInfo.point;
+            _gizmosCameraPoint = hitPosition;
+            return hitInfo.collider.gameObject == _targetObject;
         }
-        return true;
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(_gizmosCameraPoint, _sphereCastRadius);
     }
 }
