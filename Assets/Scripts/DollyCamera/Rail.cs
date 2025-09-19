63517ccd84672158ac02af7a0dab9e7c8fd74a69
Nirgigo <titouan@dupuisland.net>
2025-09-19 11:04:11 +0200

    [FIX] Bezier Rail

--- Assets/Scripts/DollyCamera/Rail.cs
+++ Assets/Scripts/DollyCamera/Rail.cs
@@ -9,8 +9,8 @@ public class Rail : MonoBehaviour
     [Header("Debug")]
     [SerializeField] private Color _railColor;
 
-    private List<Vector3> _railNodes = new List<Vector3>();
-    private List<Vector3> _bezierNodes = new List<Vector3>();
+    private List<Transform> _railNodes = new List<Transform>();
+    private List<Transform> _bezierNodes = new List<Transform>();
     private float _length;
 
     private void Start()
@@ -23,12 +23,12 @@ public class Rail : MonoBehaviour
         {
             for (int i = _railNodes.Count - 1; i > 0; i--)
             {
-                _length += Vector3.Distance(_railNodes[i], _railNodes[i - 1]);
+                _length += Vector3.Distance(_railNodes[i].position, _railNodes[i - 1].position);
             }
 
             //If loop -> add length form last to first
             if (IsLoop)
-                _length += Vector3.Distance(_railNodes[_railNodes.Count - 1], _railNodes[0]);
+                _length += Vector3.Distance(_railNodes[_railNodes.Count - 1].position, _railNodes[0].position);
         }
         else
             _length = 0;
@@ -49,9 +49,9 @@ public class Rail : MonoBehaviour
             if (child != null)
             {
                 if (!child.CompareTag("BezierNode"))
-                    _railNodes.Add(child.transform.position);
+                    _railNodes.Add(child.transform);
                 else
-                    _bezierNodes.Add(child.transform.position);
+                    _bezierNodes.Add(child.transform);
             }
         }
 
@@ -65,10 +65,12 @@ public class Rail : MonoBehaviour
                 }
                 else if (_bezierNodes.Count < _railNodes.Count)
                 {
-                    Transform _point = Instantiate(new GameObject(), _railNodes[_bezierNodes.Count] - _railNodes[_bezierNodes.Count + 1], Quaternion.Euler(Vector3.zero), transform).transform;
+                    Transform _point = Instantiate(new GameObject(),
+                                                   _railNodes[_bezierNodes.Count].position + (_railNodes[_bezierNodes.Count].position - _railNodes[_bezierNodes.Count + 1].position),
+                                                   Quaternion.Euler(Vector3.zero), transform).transform;
                     _point.tag = "BezierNode";
                     _point.name = $"Bezier [{_bezierNodes}]";
-                    _bezierNodes.Add(_point.position);
+                    _bezierNodes.Add(_point);
                 }
             }
         }
@@ -82,10 +84,12 @@ public class Rail : MonoBehaviour
                 }
                 else if (_bezierNodes.Count < _railNodes.Count - 1)
                 {
-                    Transform _point = Instantiate(new GameObject(), _railNodes[_bezierNodes.Count] - _railNodes[_bezierNodes.Count + 1], Quaternion.Euler(Vector3.zero), transform).transform;
+                    Transform _point = Instantiate(new GameObject(),
+                                                   _railNodes[_bezierNodes.Count].position + (_railNodes[_bezierNodes.Count].position - _railNodes[_bezierNodes.Count + 1].position),
+                                                   Quaternion.Euler(Vector3.zero), transform).transform;
                     _point.tag = "BezierNode";
                     _point.name = $"Bezier [{_bezierNodes}]";
-                    _bezierNodes.Add(_point.position);
+                    _bezierNodes.Add(_point);
                 }
             }
         }
@@ -108,11 +112,11 @@ public class Rail : MonoBehaviour
         for (int i = 0; i < _railNodes.Count - 1; i++)
         {
             //Calcule la longueur du segment
-            float segmentCurrentDistance = Vector3.Distance(_railNodes[i], _railNodes[i + 1]);
+            float segmentCurrentDistance = Vector3.Distance(_railNodes[i].position, _railNodes[i + 1].position);
             if (a_distance >= segmentLength && a_distance < (segmentLength + segmentCurrentDistance))
             {
                 float progressionOnSegment = (a_distance - segmentLength) / segmentCurrentDistance;
-                return Vector3.Lerp(_railNodes[i], _railNodes[i + 1], progressionOnSegment);
+                return Vector3.Lerp(_railNodes[i].position, _railNodes[i + 1].position, progressionOnSegment);
             }
             segmentLength += segmentCurrentDistance;
         }
@@ -120,15 +124,15 @@ public class Rail : MonoBehaviour
         //Si IsLoop -> connect last to first
         if (IsLoop)
         {
-            float segmentCurrentDistance = Vector3.Distance(_railNodes[_railNodes.Count - 1], _railNodes[0]);
+            float segmentCurrentDistance = Vector3.Distance(_railNodes[_railNodes.Count - 1].position, _railNodes[0].position);
             if (a_distance >= segmentLength && a_distance < (segmentLength + segmentCurrentDistance))
             {
                 float progressionOnSegment = (a_distance - segmentLength) / segmentCurrentDistance;
-                return Vector3.Lerp(_railNodes[_railNodes.Count - 1], _railNodes[0], progressionOnSegment);
+                return Vector3.Lerp(_railNodes[_railNodes.Count - 1].position, _railNodes[0].position, progressionOnSegment);
             }
             segmentLength += segmentCurrentDistance;
         }
-        return _railNodes[_railNodes.Count - 1];
+        return _railNodes[_railNodes.Count - 1].position;
     }
 
     public Vector3 GetNearestPositionFromTarget(Vector3 a_targetPosition)
@@ -141,8 +145,8 @@ public class Rail : MonoBehaviour
         Vector3 currentSmallestPosition = Vector3.zero;
         for (int i = 0; i < _railNodes.Count - 1; i++)
         {
-            float lerpPosition = MathUtils.GetNearestPointOnSegmentAsLerp(_railNodes[i], _railNodes[i + 1], a_targetPosition);
-            Vector3 projPosition = MathUtils.QuadraticBezier(_railNodes[i], _bezierNodes[i], _railNodes[i + 1], lerpPosition);
+            float lerpPosition = MathUtils.GetNearestPointOnSegmentAsLerp(_railNodes[i].position, _railNodes[i + 1].position, a_targetPosition);
+            Vector3 projPosition = MathUtils.QuadraticBezier(_railNodes[i].position, _bezierNodes[i].position, _railNodes[i + 1].position, lerpPosition);
             float distanceToTarget = Vector3.Distance(projPosition, a_targetPosition);
 
             if(distanceToTarget < currentSmallestDistance)
@@ -164,25 +168,25 @@ public class Rail : MonoBehaviour
 
         for (int i = 0; i < _railNodes.Count - 1; i++)
         {
-            Vector3 proj = MathUtils.GetNearestPointOnSegment(_railNodes[i], _railNodes[i + 1], pointOnRail);
+            Vector3 proj = MathUtils.GetNearestPointOnSegment(_railNodes[i].position, _railNodes[i + 1].position, pointOnRail);
             if ((proj - pointOnRail).sqrMagnitude <= 1e-6f)
             {
-                cumulated += Vector3.Distance(_railNodes[i], pointOnRail);
+                cumulated += Vector3.Distance(_railNodes[i].position, pointOnRail);
                 return Mathf.Clamp01(cumulated / _length);
             }
 
-            cumulated += Vector3.Distance(_railNodes[i], _railNodes[i + 1]);
+            cumulated += Vector3.Distance(_railNodes[i].position, _railNodes[i + 1].position);
         }
 
         //Si IsLoop -> connect last to first
         if (IsLoop)
         {
             int last = _railNodes.Count - 1;
-            Vector3 proj = MathUtils.GetNearestPointOnSegment(_railNodes[last], _railNodes[0], pointOnRail);
+            Vector3 proj = MathUtils.GetNearestPointOnSegment(_railNodes[last].position, _railNodes[0].position, pointOnRail);
 
             if ((proj - pointOnRail).sqrMagnitude <= 1e-6f)
             {
-                cumulated += Vector3.Distance(_railNodes[last], pointOnRail);
+                cumulated += Vector3.Distance(_railNodes[last].position, pointOnRail);
                 return Mathf.Clamp01(cumulated / _length);
             }
         }
@@ -204,19 +208,18 @@ public class Rail : MonoBehaviour
             for (int j = 1; j <= 10; j++)
             {
                 
-                Gizmos.DrawLine(MathUtils.QuadraticBezier(_railNodes[i], _bezierNodes[i], _railNodes[i + 1], (j-1) / 10f),
-                                MathUtils.QuadraticBezier(_railNodes[i], _bezierNodes[i], _railNodes[i + 1], j / 10f));
+                Gizmos.DrawLine(MathUtils.QuadraticBezier(_railNodes[i].position, _bezierNodes[i].position, _railNodes[i + 1].position, (j-1) / 10f),
+                                MathUtils.QuadraticBezier(_railNodes[i].position, _bezierNodes[i].position, _railNodes[i + 1].position, j / 10f));
             }
         }
         if (IsLoop)
         {
             for (int j = 1; j <= 10; j++)
             {
-                Gizmos.DrawLine(MathUtils.QuadraticBezier(_railNodes[_railNodes.Count - 1], _bezierNodes[_bezierNodes.Count - 1], _railNodes[0], (j - 1) / 10f),
-                                        MathUtils.QuadraticBezier(_railNodes[_railNodes.Count - 1], _bezierNodes[_bezierNodes.Count - 1], _railNodes[0], j / 10f));
+                Gizmos.DrawLine(MathUtils.QuadraticBezier(_railNodes[_railNodes.Count - 1].position, _bezierNodes[_bezierNodes.Count - 1].position, _railNodes[0].position, (j - 1) / 10f),
+                                        MathUtils.QuadraticBezier(_railNodes[_railNodes.Count - 1].position, _bezierNodes[_bezierNodes.Count - 1].position, _railNodes[0].position, j / 10f));
             }
         }
-
 
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(_playerPosOnRail, .2f);
